8f49f7f4b8ed478d5a18e5337a392dbf4167b17b
Eduardo Rezende França Silva <eduardorfs@outlook.com.br>
2021-09-16 21:39:10 -0300

    Fix spoiler of combination bug

--- src/util/emitCardsForEachSocket.ts
+++ src/util/emitCardsForEachSocket.ts
@@ -1,16 +1,35 @@
+import { Table } from './../interfaces/Table';
 import { Socket } from "socket.io";
 import { getCombination } from "../functions/getCombination";
 import { Player } from "../interfaces/Player";
 
-export function emitCardsForEachSocket(sockets: Socket[], players: Player[], tableCards?: string[]) {
-  sockets.forEach(async socket => {
-    const player = players.find(player => player.id === socket.id);
+export function emitCardsForEachSocket(table: Table, tableCards?: string[]) {
+  table.sockets.forEach(async socket => {
+    const player = table.players.find(player => player.id === socket.id);
     const cards = player?.cards;
     if (!cards) {
       return;
     }
 
-    const combination = await getCombination(cards, tableCards);
+    let combination; 
+
+    if (!table.flopStatus && !table.turnStatus && !table.riverStatus) {
+      // Não teve nenhuma carta da mesa liberada
+      combination = await getCombination(cards, []);
+    } else if (table.flopStatus && !table.turnStatus && !table.riverStatus) {
+      // Somente o flop foi liberado
+      const flop = [table.cards[0], table.cards[1], table.cards[2]];
+      combination = await getCombination(cards, flop);
+    } else if (table.flopStatus && table.turnStatus && !table.riverStatus) {
+      // Flop e trun foram liberados
+      const flop = [table.cards[0], table.cards[1], table.cards[2]];
+      const turn = flop.concat([table.cards[3]]);
+      combination = await getCombination(cards, turn);
+    } else if (table.flopStatus && table.turnStatus && table.riverStatus) {
+      combination = await getCombination(cards, tableCards);
+    };
+
+    combination = await getCombination(cards, []);
     
     socket.emit('combination', combination.descr);
     socket.emit('player', player);
